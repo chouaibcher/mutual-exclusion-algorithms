74f8f54bd3eeffeff896c442dad0b7a985b4f1da
CHERIBET CHERIF CHOUAIB <cherifchouaib.cheribet@univ-skikda.dz>
2025-04-26 17:40:51 +0100

    +3

---
+++ flag_based_mutex.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+//Algo 2
+int x=0;
+bool flag[2];
+
+void* criticalSection(){
+	x++;
+	}
+
+void* Thread1(void* arg)
+{
+do{
+label:
+	if (flag[1]){ goto label;} 
+flag[0]=true;   
+criticalSection() ;
+printf("thread 1 : %d \n",x);
+sleep(1);
+
+flag[0]=false;
+}while(x<10);
+}
+
+void* Thread2(void* arg)
+{
+do{
+label:
+if (flag[0]){
+	goto label;}
+flag[1]=true;
+ criticalSection();
+ printf("thread 2 : %d \n",x);
+sleep(1);
+
+ flag[1]=false;
+}while(1);
+}
+
+int main (void)	
+{
+
+/* Déclaration de variable de type thread */	
+     pthread_t t1;
+     pthread_t t2;
+
+/* Création et lancement des threads 1 et 2 */
+	pthread_create (&t1, NULL,Thread1, (void*)NULL);
+	pthread_create (&t2, NULL, Thread2, (void*)NULL); 
+	
+/* Attendre la fin des threads pour terminer le  main */
+     pthread_join (t1, NULL);
+     pthread_join (t2, NULL); 
+	
+	printf("  x= %d",x);
+/* Fin Normale du programme */
+     return 0;
+     }
